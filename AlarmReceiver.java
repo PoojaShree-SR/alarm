b0f4e0c2f616cae5a62c6b9c3a54818b1248fb30
PoojaShree-SR <poojashreesr07@gmail.com>
2024-10-05 04:24:34 +0530

    Create AlarmReceiver.java

---
+++ AlarmReceiver.java
@@ -0,0 +1,26 @@
+package com.example.alarmclockapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.widget.Toast;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Show a toast message when the alarm goes off
+        Toast.makeText(context, "Alarm is ringing!", Toast.LENGTH_LONG).show();
+
+        // Play the default alarm sound
+        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        if (alarmUri == null) {
+            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        }
+        Ringtone ringtone = RingtoneManager.getRingtone(context, alarmUri);
+        ringtone.play();
+    }
+}
